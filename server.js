5a992a66535693fa618142289910f0c26fbb2a3e
Rodney Julien <RodneyJulien@gmail.com>
2020-02-20 20:35:25 -0500

    remaining changes

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require('path');
+const http = require("http");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require("./app/routing/apiroutes")(app);
+require("./app/routing/htmlroutes")(app);
+
+
+app.listen(port, () => console.log("Server listening on: http://localhost:" + port));
+
+
 
